4d88678ec8254774d3acbf4a7acea35908860b4e
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2024-06-28 07:36:04 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,21 @@
-exports.home=function(req,res){
-  res.render('home');
-}
+exports.home = function(req, res) {
+    // Print request headers
+    console.log('Request Headers (home):', req.headers);
 
+    // Render the home page
+    res.render('home');
 
-exports.login=function(req,res){ 
+    // Print response headers
+    console.log('Response Headers (home):', res.getHeaders());
+};
+
+exports.login = function(req, res) {
+    // Print request headers
+    console.log('Request Headers (login):', req.headers);
+
+    // Render the login page
     res.render('login');
-}
 
+    // Print response headers
+    console.log('Response Headers (login):', res.getHeaders());
+};
